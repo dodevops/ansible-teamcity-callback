290a74d8b88b2fcee7d80c62411733a31cbb667f
Dennis Ploeger <dennis.ploeger@kps.com>
2019-04-24 13:58:01 +0200

    Optimization

--- teamcity.py
+++ teamcity.py
@@ -29,16 +29,19 @@ class CallbackModule(DefaultModule):
         self._last_play_block = None
         super(CallbackModule, self).__init__()
 
+    # Close the previously opened task block
     def _close_task_block(self):
         if self._last_task_block is not None:
             self._display.display(u"##teamcity[blockClosed name='%s']" % (self._last_task_block))
             self._last_task_block = None
 
+    # Close the previously opened play block
     def _close_play_block(self):
         if self._last_play_block is not None:
             self._display.display(u"##teamcity[blockClosed name='%s']" % (self._last_play_block))
             self._last_play_block = None
 
+    # Open a new task block
     def _print_task_banner(self, task):
         args = ''
         if not task.no_log and C.DISPLAY_ARGS_TO_STDOUT:
@@ -63,6 +66,7 @@ class CallbackModule(DefaultModule):
         self._last_task_block = u"%s [%s]" % (prefix, task_name)  
         self._last_task_banner = task._uuid
     
+    # Open a new play block
     def v2_playbook_on_play_start(self, play):
 
         self._close_task_block()
@@ -78,6 +82,7 @@ class CallbackModule(DefaultModule):
         self._last_play_block = msg
         self._play = play
 
+    # This is the last step, so previously opened tasks and plays should be closed
     def v2_playbook_on_stats(self, stats):
         self._close_task_block()
         self._close_play_block()
